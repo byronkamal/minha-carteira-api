5726d4dc7b8f5281b949f9e500acd38877e27c24
byronkamal <byronkamal@hotmail.com>
2023-01-22 19:47:52 -0300

    testes

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,13 +1,27 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import { createConnection, getConnectionOptions } from 'typeorm';
 
 interface IOptions {
   host: string;
 }
 
-getConnectionOptions().then((options) => {
+getConnectionOptions().then(options => {
   const newOptions = options as IOptions;
-  newOptions.host = "tecnospeed_db_service";
+  newOptions.host = 'localhost';
   createConnection({
     ...options,
   });
 });
+
+// import { DataSource } from 'typeorm';
+
+// const AppDataSource = new DataSource({
+//   type: 'postgres',
+//   host: 'localhost',
+//   username: 'admin',
+//   password: 'admin',
+//   database: 'db_teste',
+// });
+
+// AppDataSource.initialize()
+//   .then(() => console.log('Data Source has been initialized!'))
+//   .catch(() => console.log('Error during Data Source initialization'));
